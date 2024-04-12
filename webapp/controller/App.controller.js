f1306465f36c3b93e8ce26c8df37ad18ccd3cd9a
Raoni <raoniks@gmail.com>
2024-04-11 22:16:22 -0300

    aula 2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        'sap/ui/model/json/JSONModel'
     ],
     function(BaseController) {
       "use strict";
-  
+
       return BaseController.extend("zapprks1.controller.App", {
         onInit: function() {
         }
